8c670980c4edd0864cd7dd69fbe13ffed6dbbb0f
Gabriel Marquette <gab64320@gmail.com>
2023-06-09 13:49:00 +0200

    Qualité Fragment

--- GuessWho/app/src/main/java/fr/gmarquette/guesswho/gui/FirstAnswerFirstColumn.java
+++ GuessWho/app/src/main/java/fr/gmarquette/guesswho/gui/FirstAnswerFirstColumn.java
@@ -18,33 +18,15 @@ import fr.gmarquette.guesswho.R;
  */
 public class FirstAnswerFirstColumn extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private View view;
 
     public FirstAnswerFirstColumn() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirstAnswerFirstColumn.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FirstAnswerFirstColumn newInstance(String param1, String param2) {
+    public static FirstAnswerFirstColumn newInstance() {
         FirstAnswerFirstColumn fragment = new FirstAnswerFirstColumn();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,31 +34,19 @@ public class FirstAnswerFirstColumn extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
        view = inflater.inflate(R.layout.fragment_first_answer_first_column, container, false);
-        return view;
+       return view;
     }
 
-    View view;
-        private TextView textView;
-
-
-        public void updateText(String newText) {
-            textView = view.findViewById(R.id.test);
-            if (textView != null) {
-                textView.setText(newText);
-            }
+    public void updateText(String newText) {
+        TextView textView = view.findViewById(R.id.test);
+        if (textView != null) {
+            textView.setText(newText);
         }
-
-
-
+    }
 }
