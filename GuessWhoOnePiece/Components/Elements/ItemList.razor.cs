7ba57f57d99abfe7519546849df5467073813cbc
Gabriel Marquette <gab64320@gmail.com>
2024-09-08 10:24:18 +0200

    ListCharacter Page + Component ItemList (Not Working)

---
+++ GuessWhoOnePiece/Components/Elements/ItemList.razor.cs
@@ -0,0 +1,9 @@
+﻿using GuessWhoOnePiece.Model.Characters;
+using Microsoft.AspNetCore.Components;
+
+namespace GuessWhoOnePiece.Components.Elements;
+
+public partial class ItemList(Character character) : ComponentBase
+{
+    [Parameter] public Character Character { get; set; } = character;
+}
---
+++ GuessWhoOnePiece/Components/Pages/ListCharacters.razor.cs
@@ -0,0 +1,24 @@
+﻿using GuessWhoOnePiece.Model.Characters;
+using Microsoft.AspNetCore.Components;
+
+namespace GuessWhoOnePiece.Components.Pages;
+
+public partial class ListCharacters : ComponentBase
+{
+    private readonly List<Character> characters = new List<Character>
+    {
+        new Character("Katakuri", false, "",
+            0, "", false, 48, "", "", 0),
+        new Character("Marco", false, "",
+            0, "", false, 37, "", "", 0),
+        new Character("Ener", false, "",
+            0, "", false, 33, "", "", 0),
+        new Character("Brulée", false, "",
+            0, "", false, 45, "", "", 0),
+        new Character("Tama", false, "",
+            0, "", false, 17, "", "", 0),
+        new Character("Luffy", false, "",
+            0, "", false, 23, "", "", 0)
+    };
+
+}
