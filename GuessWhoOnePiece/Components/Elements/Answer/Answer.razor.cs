a259d7401f323ad4b9734f258ad11039413cb6ec
Gabriel Marquette <gab64320@gmail.com>
2024-09-18 19:54:07 +0200

    Fix Carousel List

--- GuessWhoOnePiece/Components/Elements/Answer/Answer.razor.cs
+++ GuessWhoOnePiece/Components/Elements/Answer/Answer.razor.cs
@@ -4,7 +4,29 @@ namespace GuessWhoOnePiece.Components.Elements.Answer
 {
     public partial class Answer : ComponentBase
     {
+        [Parameter] public string? AnswerText { get; set; }
         
+        // Correct, Wrong, Not Answered
+        [Parameter] public AnswerState? AnswerState { get; set; }
+
+        private void DefineColor()
+        {
+            switch (AnswerState)
+            {
+                case Elements.Answer.AnswerState.Correct:
+                case Elements.Answer.AnswerState.Wrong:
+                case Elements.Answer.AnswerState.NotAnswered:
+                default:
+                    break;
+            }
+        }
+    }
+
+    public enum AnswerState
+    {
+        Correct = 0, 
+        Wrong = 1, 
+        NotAnswered = 2,
     }
 }
 
--- GuessWhoOnePiece/Components/Elements/CarouselAnswer/CarouselAnswer.razor.cs
+++ GuessWhoOnePiece/Components/Elements/CarouselAnswer/CarouselAnswer.razor.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using GuessWhoOnePiece.Model.Characters;
+using Microsoft.AspNetCore.Components;
 
 namespace GuessWhoOnePiece.Components.Elements.CarouselAnswer;
 
 public partial class CarouselAnswer : ComponentBase
 {
+    [Parameter] public Character? Character { get; set; }
 }
