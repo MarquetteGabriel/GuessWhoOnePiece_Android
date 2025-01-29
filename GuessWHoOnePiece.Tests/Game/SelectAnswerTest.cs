ac4b74e2aed70e99de71ae756a7515733497136f
Gabriel Marquette <gab64320@gmail.com>
2025-01-29 15:28:26 +0100

    Adding SelectAnswerSystem

---
+++ GuessWHoOnePiece.Tests/Game/SelectAnswerTest.cs
@@ -0,0 +1,32 @@
+﻿using GuessWhoOnePiece.Model.Characters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GuessWhoOnePiece.Model.Game;
+
+namespace GuessWhoOnePiece.Tests.Game
+{
+    public class SelectAnswerTest
+    {
+        [Fact]
+        public void Test_SelectAnswerCharacter()
+        {
+            var characterSearched = new Character("Monkey D. Luffy", true, "3 Md", 1, "Pirate", true, 19, "Straw Hat Pirates", "", 0);
+            var character = new Character("Roronoa Zoro", false, "2 Md", 1, "Pirate", true, 21, "Straw Hat Pirates", "", 0);
+
+            var selectAnswer = new SelectAnswer(characterSearched);
+            selectAnswer.SelectAnswerCharacter(character);
+
+            Assert.Equal(Model.AnswerState.Correct, character.AnswerStateList!.Alive);
+            Assert.Equal(Model.AnswerState.Wrong, character.AnswerStateList!.Age);
+            Assert.Equal(Model.AnswerState.Correct, character.AnswerStateList!.Type);
+            Assert.Equal(Model.AnswerState.Correct, character.AnswerStateList!.FirstAppearance);
+            Assert.Equal(Model.AnswerState.Wrong, character.AnswerStateList!.Bounty);
+            Assert.Equal(Model.AnswerState.Correct, character.AnswerStateList!.Crew);
+            Assert.Equal(Model.AnswerState.Wrong, character.AnswerStateList!.Name);
+            Assert.Equal(Model.AnswerState.Wrong, character.AnswerStateList!.DevilFruit);
+        }
+    }
+}
---
+++ GuessWhoOnePiece.Model/Game/SelectAnswer.cs
@@ -0,0 +1,86 @@
+﻿using GuessWhoOnePiece.Model.Characters;
+
+namespace GuessWhoOnePiece.Model.Game
+{
+    public class SelectAnswer(Character charcaterSearched)
+    {
+        private readonly JudgementAnswer _judgementAnswer = new(charcaterSearched);
+
+        internal Character SelectAnswerCharacter(Character character)
+        {
+            character = SelectAnswerAlive(character);
+            character = SelectAnswerAge(character);
+            character = SelectAnswerType(character);
+            character = SelectAnswerChapter(character);
+            character = SelectAnswerBounty(character);
+            character = SelectAnswerCrew(character);
+            character = SelectAnswerName(character);
+            character = SelectAnswerDevilFruit(character);
+            return character;
+        }
+
+        private Character SelectAnswerAlive(Character character)
+        {
+            var answerAlive = _judgementAnswer.IsAlive(character);
+            character.AnswerStateList!.Alive = AnswerResult.SetAnswerStateBoolean(answerAlive);
+            character.AnswerImageLink!.Alive = DefinePictures.SetAlivePicture(answerAlive);
+            return character;
+        }
+
+        private Character SelectAnswerAge(Character character)
+        {
+            var answerAge = _judgementAnswer.IsOlder(character);
+            character.AnswerStateList!.Age = AnswerResult.SetAnswerStateAgeType(answerAge);
+            character.AnswerImageLink!.Age = DefinePictures.SetAgePicture(answerAge);
+            return character;
+        }
+
+        private Character SelectAnswerType(Character character)
+        {
+            var answerType = _judgementAnswer.WhatOccupation(character);
+            character.AnswerStateList!.Type = AnswerResult.SetAnswerStateBoolean(answerType);
+            character.AnswerImageLink!.Type = DefinePictures.SetTypePicture(character.Type);
+            return character;
+        }
+
+        private Character SelectAnswerChapter(Character character)
+        {
+            var answerChapter = _judgementAnswer.IsNewer(character);
+            character.AnswerStateList!.FirstAppearance = AnswerResult.SetAnswerStateChapterType(answerChapter);
+            character.AnswerImageLink!.FirstAppearance = DefinePictures.SetChapterPicture(answerChapter);
+            return character;
+        }
+
+        private Character SelectAnswerBounty(Character character)
+        {
+            var answerBounty = _judgementAnswer.WantedBounty(character);
+            character.AnswerStateList!.Bounty = AnswerResult.SetAnswerStateBountyType(answerBounty);
+            character.AnswerImageLink!.Bounty = DefinePictures.SetBountyPicture(answerBounty);
+            return character;
+        }
+
+        private Character SelectAnswerCrew(Character character)
+        {
+            var answerCrew = _judgementAnswer.WhatCrew(character);
+            character.AnswerStateList!.Crew = AnswerResult.SetAnswerStateBoolean(answerCrew);
+            character.AnswerImageLink!.Crew = DefinePictures.SetCrewPictures(character.Crew);
+            return character;
+        }
+
+        private Character SelectAnswerName(Character character)
+        {
+            var answerName = _judgementAnswer.IsSameName(character);
+            character.AnswerStateList!.Name = AnswerResult.SetAnswerStateBoolean(answerName);
+            character.AnswerImageLink!.Name = string.Empty;
+            return character;
+        }
+
+        private Character SelectAnswerDevilFruit(Character character)
+        {
+            var answerDevilFruit = _judgementAnswer.HasEatenDevilFruit(character);
+            character.AnswerStateList!.DevilFruit = AnswerResult.SetAnswerStateBoolean(answerDevilFruit);
+            character.AnswerImageLink!.DevilFruit = DefinePictures.SetDevilFruitPicture(answerDevilFruit);
+            return character;
+        }
+    }
+}
